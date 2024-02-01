bf93cf74cc13ae4a9546d143a2cbb27e7b3cf285
cornnick <ntcorn@ualr.edu>
2024-02-01 18:04:57 +0000

    practice and homework

---
+++ Homework 1/homework1.py
@@ -0,0 +1,4 @@
+value1 = input("Enter an Integer:")
+value2 = input("Enter an Integer:")
+
+value1
---
+++ Practice 1/practice1.py
@@ -0,0 +1,28 @@
+print("Hello World.")
+
+import math
+x=16
+y=2
+print(math.sqrt(x))
+
+z=5
+print(y+z)
+
+myname = "Nicholas"
+print(myname)
+
+userinput = input("Enter your name:")
+print("Hello,", userinput)
+
+isgreater = y > z
+isless = y < z
+isequal = y == z
+
+print(isgreater)
+print(isless)
+print(isequal)
+
+a = 5
+b = 3
+a &= b  # This is equivalent to a = a & b
+print(a)  # Output: 1
